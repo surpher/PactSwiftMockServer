9919cfe936d9f1119fed27f2cd869f9d50e6f658
Marko Justinek <marko.justinek@ittybittyapps.com>
2021-05-13 11:09:04 +1000

    chore: Update MockServer version and expose as XCFramework

--- Package.swift
+++ Package.swift
@@ -3,38 +3,33 @@
 import PackageDescription
 
 let package = Package(
+
 	name: "PactSwiftMockServer",
+
 	platforms: [
 		.macOS(.v10_12),
 		.iOS(.v12)
 	],
+
 	products: [
 		.library(
 			name: "PactSwiftMockServer",
 			targets: ["PactSwiftMockServer"]
 		)
 	],
+
 	dependencies: [
 		.package(url: "https://github.com/surpher/PactMockServer.git", from: "0.0.1-beta"),
 		.package(url: "https://github.com/surpher/PactSwiftToolbox.git", from: "0.1.0")
 	],
+
 	targets: [
-		.target(
+		.binaryTarget(
 			name: "PactSwiftMockServer",
-			dependencies: [
-				"PactMockServer",
-				"PactSwiftToolbox"
-			],
-			path: "./Sources"
-		),
-		.testTarget(
-			name: "PactSwiftMockServerTests",
-			dependencies: [
-				"PactSwiftMockServer",
-				"PactSwiftToolbox"
-			],
-			path: "./Tests"
+			path: "PactSwiftMockServer.xcframework"
 		),
 	],
+
 	swiftLanguageVersions: [.v5]
+
 )

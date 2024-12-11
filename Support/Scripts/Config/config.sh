1cd1b34c3c5feea52d3dbaeb6869729c9b432032
Marko Justinek <1733327+surpher@users.noreply.github.com>
2024-12-11 12:56:50 +1100

    Only run GHAs on pushes to run-on-ci/**

--- Support/Scripts/Config/config.sh
+++ Support/Scripts/Config/config.sh
@@ -8,13 +8,17 @@ XCODE_VERSION_MIN_SUGGESTED="16.1"
 XCODE_VERSION_MIN_SUPPORTED="16.0"
 
 # Project configuration
-CONFIGURATION_FILE="Configurations/Project-Shared.xcconfig"
-CHANGE_LOG="CHANGELOG.md"
-TAG_MESSAGE_FILE="TAG_MESSAGE_FILE.md"
-XCFRAMEWORK_NAME="PactSwiftMockServer.xcframework"
+XCPRODUCT_NAME="PactSwiftMockServer"
+XCFRAMEWORK_DIR="Framework"
+XCFRAMEWORK_LOCATION="Framework"
 
-REMOTE_NAME="PactSwiftServer"
-RELEASE_REPO_NAME="PactSwiftServer"
-DEFAULT_REPO_NAME="PactSwiftMockServer"
-REPO_OWNER="surpher"
-REMOTE_REPO_BASE="git@github.com:$REPO_OWNER"
+CONFIGURATION_FILE="Configurations/Project-Shared.xcconfig" # The file where the marketing version is updated before building XCFramework
+CHANGE_LOG="CHANGELOG.md" # Self explanatory
+TAG_MESSAGE_FILE="TAG_MESSAGE_FILE.tmp" # The temporary file to keep the changes in
+XCFRAMEWORK_NAME="$XCPRODUCT_NAME.xcframework" # The name of the XCFramework as it will be shared
+
+REMOTE_NAME="PactSwiftServer" # The name of the locally set git remote
+RELEASE_REPO_NAME="PactSwiftServer" # The repo name to which a new release is being pushed
+DEFAULT_REPO_NAME="PactSwiftMockServer" # The repo name where the source code is
+REPO_OWNER="surpher" # Project owner on GitHub
+REMOTE_REPO_BASE="git@github.com:$REPO_OWNER" # The SSH URI to the project owner's space
